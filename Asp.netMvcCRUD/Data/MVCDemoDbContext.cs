70c39a0b669ea60645ba2afccf95da99da74a7c3
diyorbek00013@gmail.com <diyorbek00013@gmail.com>
2022-09-30 18:41:58 +0500

    Add project files.

---
+++ Asp.netMvcCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Asp.netMvcCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.netMvcCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
